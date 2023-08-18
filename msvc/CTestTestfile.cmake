28cff8725a8e0ca0c49c3e2c97ecf9af58f43140
gajgeospatial <glen.johnson@gaj-geospatial.com>
2023-08-17 20:14:48 -0500

    Initial check in

---
+++ msvc/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: N:/Development/Dev_Base/xerces-c-3.2.4
+# Build directory: N:/Development/Dev_Base/xerces-c-3.2.4/msvc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("src")
+subdirs("tests")
+subdirs("samples")
---
+++ msvc/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: N:/Development/Dev_Base/xerces-c-3.2.4/src
+# Build directory: N:/Development/Dev_Base/xerces-c-3.2.4/msvc/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
